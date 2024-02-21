59253605a42a25a2bc5c154df407ef6434f01bd2
Seungbin Oh <sboh1214@gmail.com>
2024-02-21 14:27:02 +0900

    feat: add 2023 autumn winter volume

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,43 @@ export default async function Home() {
   return (
     <Box w="100%" mx="72px">
       <Heading mt="72px" mb="36px">
+        2023년 가을겨울호
+      </Heading>
+      <Wrap w="100%" align="center" spacing="24px">
+        {articles
+          .filter((article) => article.volume === "23-wintera")
+          .map((article: Article, index: number) => {
+            let imageUrl;
+            if (article.header.image) {
+              imageUrl = article.header.image;
+            } else {
+              const firstImage = article.body.find(
+                (item) => item.type === "image"
+              ) as ArticleImage;
+              imageUrl = firstImage?.image;
+            }
+
+            return (
+              <WrapItem key={index}>
+                <Link href={`/article/${article.volume}/${article.index}`}>
+                  <Card w="sm" h="xl">
+                    <CardBody>
+                      <AspectRatio ratio={1}>
+                        <Image src={imageUrl} borderRadius="lg" />
+                      </AspectRatio>
+                      <Stack mt="6" spacing="3">
+                        <Heading size="sm">{article.category}</Heading>
+                        <Heading size="md">{article.header.title}</Heading>
+                        <Text>{article.header.author}</Text>
+                      </Stack>
+                    </CardBody>
+                  </Card>
+                </Link>
+              </WrapItem>
+            );
+          })}
+      </Wrap>
+      <Heading mt="72px" mb="36px">
         2023년 여름호
       </Heading>
       <Wrap w="100%" align="center" spacing="24px">
