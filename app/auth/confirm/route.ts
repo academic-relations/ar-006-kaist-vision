437838c27acdddc19e6341a1a91552dbf581f1c0
Seungbin Oh <sboh1214@gmail.com>
2024-07-23 04:00:33 +0900

    bug: fix login redirect link

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -2,13 +2,13 @@ import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest } from "next/server";
 import { redirect } from "next/navigation";
 import { createServerSupabase } from "../../../utils/supabase/server";
-import { cookies } from "next/headers";
+import { Links } from "../../../utils/utils";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/admin";
+  const next = searchParams.get("next") ?? Links.adminVolumes;
 
   if (token_hash && type) {
     const supabase = createServerSupabase();
